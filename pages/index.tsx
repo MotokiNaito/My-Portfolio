ce6114f14a6e1562bc0ac43e8558526ad7b8c712
Motoki Naito <motoki.developer@gmail.com>
2019-07-15 22:11:08 -0700

    add common css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,8 @@
-const IndexPage = () => <h1>Testing Next.js App written in TypeScript with Jest</h1>;
-
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Testing Next.js App written in TypeScript with Jest</h1>
+    </div>
+  );
+};
 export default IndexPage;
