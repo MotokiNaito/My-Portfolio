3372225d0dc540b23dbce2183199e644c4443a8a
Motoki Naito <motoki.developer@gmail.com>
2019-07-24 21:05:31 -0700

    add header

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,25 +2,41 @@ import React from "react";
 
 const IndexPage = () => {
   return (
-    <main className="home">
-      <div className="home-inner">
-        <section className="about panel">
-          <h1 className="about__fname">Motoki</h1>
-          <h1 className="about__lname">Naito</h1>
-          <h3 className="about__title">
-            Web Developer <br />
-            and Designer
-          </h3>
-        </section>
-        <div className="line-container">
-          <div className="line line--first"></div>
-          <div className="line line--second"></div>
-          <div className="line line--third"></div>
-          <div className="line line--fourth"></div>
-          <div className="line line--fifth"></div>
+    <>
+      <header className="header">
+        <nav className="main-nav">
+          <ul>
+            <li className="main-nav__list">About</li>
+            <li className="main-nav__list">Projects</li>
+            <li className="main-nav__list">Contact</li>
+          </ul>
+        </nav>
+      </header>
+      <main className="home">
+        <div className="home-inner">
+          <section className="about panel">
+            <h1 className="about__fname">Motoki</h1>
+            <h1 className="about__lname">Naito</h1>
+            <h3 className="about__title">
+              Front End Developer
+              <br />
+              Based in Vancouver
+            </h3>
+            <div className="about__img">
+              <div className="bg-image" style={{ backgroundImage: `url(${"https://via.placeholder.com/1000"})` }} />
+            </div>
+          </section>
+          <div className="line-container">
+            <div className="line line--first"></div>
+            <div className="line line--second"></div>
+            <div className="line line--third"></div>
+            <div className="line line--fourth"></div>
+            <div className="line line--fifth"></div>
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 };
+
 export default IndexPage;
